dd118fb435fc1d39ff9fc3e8974b4ac810fc9cf1
Kunal Hedaoo <kunalhedaoo25@gmail.com>
2021-11-09 10:07:59 -0500

    getting_start_with_julia

---
+++ getting_start_with_julia.jl
@@ -0,0 +1,51 @@
+5+3
+
+x = 3 + 4im
+
+typeof(x)
+
+real(x)
+
+imag(x)
+
+x = 4 + 5im
+
+angle(x)
+
+abs(x)
+
+(4+5im)*2
+
+(2+3im)*(4+5im)
+
+complex(2,3)
+
+'A'
+
+typeof('A')
+
+'\u2288'
+
+'\U2665'
+
+'\U260E'
+
+Int('A')
+
+Int('☎')
+
+"hello world"
+
+text = "hello world"
+
+text[2]
+
+text[end]
+
+text[end-1]
+
+x = """hello world"""
+
+x[1:5]
+
+x[7:end]
